2f17ab43535fe13723f6440418e3c2e1b682d01a
Giedre Genute <giedre.genute@gmail.com>
2019-05-23 17:25:53 -0500

    created react app, added palette

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import seedColors from './seedColors';
+import './App.css';
+import Palette from './Palette';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Palette.css';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette" >
+        {/* Navbar goes here */}
+        <div className="Palette-colors">
+          {/* Bunch of color boxes */}
+        </div>
+        {/* Footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
