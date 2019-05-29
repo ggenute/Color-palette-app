049d40c41567f313cdd4f87b6f70648d01ee3d05
Giedre Genute <giedre.genute@gmail.com>
2019-05-29 15:35:14 -0500

    added link to ColorBox and added route

--- src/App.js
+++ src/App.js
@@ -30,8 +30,12 @@ class App extends Component {
             />
           )}
         />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>SINGLE COLOR PAGE!</h1>}
+        />
       </Switch>
-      // <Palette palette={generatePalette(seedColors[0])} />
     );
   }
 }
