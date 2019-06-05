01adbc93223db72e223f357712c167be953caa8f
Giedre Genute <giedre.genute@gmail.com>
2019-06-04 19:09:18 -0500

    refactored MiniPalettes rendering

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -31,6 +31,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
   openDialog(id) {
     this.setState({ openDeleteDialog: true, deletetingId: id });
@@ -64,7 +65,7 @@ class PaletteList extends Component {
               >
                 <MiniPalette
                   {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
+                  goToPalette={this.goToPalette}
                   // handleDelete={deletePalette}
                   openDialog={this.openDialog}
                   key={palette.id}
