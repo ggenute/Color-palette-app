929972a331bcef5e8c1c788e0e73075182d1f554
Giedre Genute <giedre.genute@gmail.com>
2019-06-04 20:12:06 -0500

    fixed issues with NewPaletteForm

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -57,7 +57,7 @@ class ColorPickerForm extends Component {
           className={classes.picker}
         />
 
-        <ValidatorForm onSubmit={this.handleSubmit}>
+        <ValidatorForm onSubmit={this.handleSubmit} instantValidate={false}>
           <TextValidator
             value={newColorName}
             name='newColorName'
